cc2ea2eba52c3bfa2b927193bc5edd28aa8d77cd
Ryan Parker <ryanp@bbd.co.za>
2023-02-23 16:30:09 +0200

    Fix typo in tutors.js

--- src/pages/tutors.js
+++ src/pages/tutors.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-export default funciton Tutors() {
+export default function Tutors() {
     return (
         <div>
             <Head>
